2afb7e9aa92e89fbc1098b8e88fcb00f7b21be42
Isabella <idavila2021@gmail.com>
2024-08-10 21:21:36 +0100

    style(Button)

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -11,7 +11,7 @@ export const Button: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
-      className={`px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ${className}`}
+      className={className= `shadow-[0_0_0_3px_#000000_inset] px-6 py-2 bg-transparent border border-black dark:border-white dark:text-white text-black rounded-lg font-bold transform hover:-translate-y-1 transition duration-400" ${className}`}
       {...props}
     >
       {children}
