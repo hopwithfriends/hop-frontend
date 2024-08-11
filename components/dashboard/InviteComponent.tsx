3651d186a236786ec8e4c39e1e05ab8afed76dcb
Isabella <idavila2021@gmail.com>
2024-08-11 21:08:40 +0100

    fix(invite): pass pfp prop

--- components/dashboard/InviteComponent.tsx
+++ components/dashboard/InviteComponent.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import { IoCheckbox } from "react-icons/io5";
 import { FaRectangleXmark } from "react-icons/fa6";
+import Image from "next/image";
 interface InviteProps {
   spaceName: string;
   username: string;
+  pfp: string;
 }
 
-const InviteComponent: React.FC<InviteProps> = ({spaceName, username}) => {
+const InviteComponent: React.FC<InviteProps> = ({spaceName, username, pfp}) => {
 
   username = "Username"
   spaceName= "Space name"
+  pfp= "/PFP.jpg"
   
   return (
     <div className="w-[99%] mt-2 mb-3 bg-gray-700 rounded-xl h-12 flex items-center px-2">
-      <div className="w-10 h-10 bg-gray-500 rounded-full flex-shrink-0"></div>
+      <div className="relative w-10 h-10 rounded-full flex-shrink-0 overflow-hidden">
+      <Image
+            src={pfp}
+            alt="Member Icon"
+            layout="fill"
+            objectFit="cover"
+          />
+      </div>
       <div className="flex-1 flex flex-col justify-center ml-3">
         <div className="flex items-center space-x-2">
           <div className="text-white font-semibold text-sm">{username}</div>
