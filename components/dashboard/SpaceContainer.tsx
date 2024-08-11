9819baf0ac80e5be01a03eb65490f5de70d32911
Isabella <idavila2021@gmail.com>
2024-08-11 10:37:02 +0100

    style(dash): profile icon in top right

--- components/dashboard/SpaceContainer.tsx
+++ components/dashboard/SpaceContainer.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import Link from "next/link";
-import { Button } from "@components/ui/Button";
 import CreateSpaceButton from "@components/dashboard/CreateSpaceButton";
 import { SpaceComponent } from "@components/dashboard/SpaceComponent";
+import { CgProfile } from "react-icons/cg";
 
 const SpaceContainer = () => {
   return (
     <main className="flex-1 p-6 relative">
       <h1 className="text-5xl font-bold mb-10 mt-5">My Spaces</h1>
 
+      <div className="absolute top-10 right-11">
+        <Link href="/profile" passHref>
+          <CgProfile className="text-5xl cursor-pointer" /> 
+        </Link>
+      </div>
+
       <div className="mb-6 flex space-x-4">
         <CreateSpaceButton />
-        
-        <nav className="flex space-x-4"> 
+        <nav className="flex space-x-4">
           <Link href="/space/id" passHref>
             <div>
               <SpaceComponent />
@@ -25,7 +30,6 @@ const SpaceContainer = () => {
           </Link>
         </nav>
       </div>
-
     </main>
   );
 };
--- components/layout/LeftSidebar.tsx
+++ components/layout/LeftSidebar.tsx
@@ -6,19 +6,14 @@ import OnlineFriendsContainer from './OnlineFriendsContainer';
 const LeftSidebar = () => {
   return (
     <div className="bg-gradient-to-b from-gray-600 to-gray-800 text-white p-6 w-80 h-screen flex flex-col shadow-xl rounded-r-lg">
-      <Logo />
+      <Logo className="mb-6 mx-auto w-36" />
       <nav className="flex-1 flex flex-col items-center">
-        <div>
-        <Link href="/profile" passHref>
-          <Button className= "w-32 font-extrabold">Profile</Button>
-        </Link>
         <Link href="/friends">
-          <Button className="w-32 mt-2 ml-2 font-extrabold">
+          <Button className="w-64 mt-1">
             Friends
           </Button>
         </Link>
-        </div>
-        <p className="text-2xl font-bold text-gray-200 mt-7 mb-1">Online Friends</p>
+        <p className="text-2xl font-bold text-gray-200 mt-5 mb-1">Online Friends</p>
         <OnlineFriendsContainer />
       </nav>
     </div>
