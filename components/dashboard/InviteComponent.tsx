1bc0ded7a9433716701a9a00b1e5a31dd1d917c4
Isabella <idavila2021@gmail.com>
2024-08-11 22:05:35 +0100

    fix(invites)

--- components/dashboard/InviteComponent.tsx
+++ components/dashboard/InviteComponent.tsx
@@ -1,43 +1,88 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { IoCheckbox } from "react-icons/io5";
 import { FaRectangleXmark } from "react-icons/fa6";
 import Image from "next/image";
 interface InviteProps {
+  id: string;
   spaceName: string;
   username: string;
   pfp: string;
+  onHandle: (id: string) => void;
 }
 
-const InviteComponent: React.FC<InviteProps> = ({spaceName, username, pfp}) => {
+const InviteComponent: React.FC<InviteProps> = ({
+  id,
+  spaceName,
+  username,
+  pfp,
+  onHandle,
+}) => {
+  const [add, setAdd] = useState(false);
+  const [decline, setDecline] = useState(false);
+  const [handled, setHandled] = useState(false);
 
-  username = "Username"
-  spaceName= "Space name"
-  pfp= "/PFP.jpg"
-  
+  const handleAdd = async () => {
+    setAdd(true);
+
+    /*
+    try {
+      const response = await fetch("/newroom", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          spaceName: spaceName,
+          username: username,
+          pfp: pfp,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to post data");
+      }
+    } catch (error) {
+      console.error("Error posting data:", error);
+    }
+      */
+    setHandled(true);
+    onHandle(id);
+  };
+
+  const handleDecline = () => {
+    setDecline(true);
+    setHandled(true);
+    onHandle(id);
+  };
+
   return (
-    <div className="w-[99%] mt-2 mb-3 bg-gray-700 rounded-xl h-12 flex items-center px-2">
-      <div className="relative w-10 h-10 rounded-full flex-shrink-0 overflow-hidden">
-      <Image
+    !handled && (
+      <div className="w-[99%] mt-2 mb-3 bg-gray-700 rounded-xl h-12 flex items-center px-2">
+        <div className="relative w-10 h-10 rounded-full flex-shrink-0 overflow-hidden">
+          <Image
             src={pfp}
             alt="Member Icon"
-            layout="fill"
-            objectFit="cover"
+            fill
+            style={{ objectFit: "cover" }}
           />
-      </div>
-      <div className="flex-1 flex flex-col justify-center ml-3">
-        <div className="flex items-center space-x-2">
-          <div className="text-white font-semibold text-sm">{username}</div>
         </div>
-        <div className="text-gray-400 text-xs">{spaceName}</div>
-      </div>
-      <button>
-        <IoCheckbox className="text-2xl mr-1" />
-      </button>
+        <div className="flex-1 flex flex-col justify-center ml-3">
+          <div className="flex items-center space-x-2">
+            <div className="text-white font-semibold text-sm">{spaceName}</div>
+          </div>
+          <div className="text-gray-400 text-xs">{username}</div>
+        </div>
+        <button onClick={handleAdd}>
+          <IoCheckbox className="text-2xl mr-1" />
+        </button>
 
-      <button>
-        <FaRectangleXmark className="text-[22px]" />
-      </button>
-    </div>
+        <button onClick={handleDecline}>
+          <FaRectangleXmark className="text-[22px]" />
+        </button>
+      </div>
+    )
   );
 };
 
