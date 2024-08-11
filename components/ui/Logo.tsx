33a922caa1d4e15e5bada29c7ee07c8ef0942aec
Isabella Davila <idavila2021@gmail.com>
2024-08-11 22:21:08 +0100

    Fix/invites (#10)
    * fix(invite): pass props and delete green dot
    
    * fix(invite): pass pfp prop
    
    * fix(onlineFriends): pass pfp prop
    
    * fix(invites)
    
    * fix(invite)
    
    * fix(invite): change comment

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,18 +1,18 @@
 import Image from "next/image";
 interface LogoProps {
-	className?: string;
+  className?: string;
 }
 
 export const Logo: React.FC<LogoProps> = () => {
-	return (
-		<Image
-			src="/images/Logo.webp"
-			alt="Logo"
-			objectFit="contain"
-			width={100}
-			height={100}
-			priority
-			className="mx-auto rounded-full mb-5"
-		/>
-	);
+  return (
+    <Image
+      src="/images/Logo.webp"
+      alt="Logo"
+      width={100}
+      height={100}
+      style={{ objectFit: "contain" }} 
+      priority
+      className="mx-auto rounded-full mb-5"
+    />
+  );
 };
