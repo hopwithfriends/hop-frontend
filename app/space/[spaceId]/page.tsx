8fa46382ef7d5521aebab7905854b72827b8c5a0
Isabella <idavila2021@gmail.com>
2024-08-12 20:38:47 +0100

    style: clean up code and remove button

--- app/space/[spaceId]/page.tsx
+++ app/space/[spaceId]/page.tsx
@@ -6,26 +6,6 @@ import BottomBar from "@components/space/BottomBar";
 import CursorContainer from "@components/space/CursorContainer";
 import SetNickname from "@components/space/SetNickname";
 
-const handleClick = () => {
-	console.log("clicked");
-};
-
-/*
-<iframe
-						className="absolute inset-0 w-full h-full z-10"
-						title="vnc"
-						src="https://vnc-socket-2.fly.dev/"
-					/>
-
-					 <button
-                  type="button"
-                  className="bg-black text-white ml-20 pointer-events-auto"
-                  onClick={handleClick}
-                >
-                  Click
-                </button>
-*/
-
 const SpacePage = () => {
 	const [username, setUsername] = useState("");
 	const [color, setColor] = useState("");
@@ -44,13 +24,11 @@ const SpacePage = () => {
 					) : (
 						<SetNickname onSubmit={setUsername} setColorProp={setColor} />
 					)}
-					<button
-						type="button"
-						className="bg-black text-white ml-40 w-30 h-12 z-30"
-						onClick={handleClick}
-					>
-						Click
-					</button>
+					<iframe
+						className="absolute inset-0 w-full h-full z-10"
+						title="vnc"
+						src="https://vnc-socket-2.fly.dev/"
+					/>
 				</div>
 				<BottomBar />
 			</main>
