e3a4f844eb9e114864392ba71df59fd75bd2ff52
Isabella <idavila2021@gmail.com>
2024-08-12 15:52:37 +0100

    fix(space): iframe clickable with cursors

--- app/space/[spaceId]/page.tsx
+++ app/space/[spaceId]/page.tsx
@@ -6,29 +6,47 @@ import BottomBar from "@components/space/BottomBar";
 import CursorContainer from "@components/space/CursorContainer";
 import SetNickname from "@components/space/SetNickname";
 
-const SpacePage = () => {
-	const [username, setUsername] = useState("");
-	const [color, setColor] = useState("");
-
-	return (
-		<div className="flex h-screen overflow-hidden">
-			<main className="flex-grow flex flex-col overflow-hidden relative">
-				<div className="relative flex-grow">
-					{username ? (
-						<CursorContainer username={username} color={color} />
-					) : (
-						<SetNickname onSubmit={setUsername} setColorProp={setColor} />
-					)}
-					<iframe
+/*
+<iframe
 						className="absolute inset-0 w-full h-full z-10"
 						title="vnc"
 						src="https://vnc-socket-2.fly.dev/"
 					/>
-				</div>
-				<BottomBar />
-			</main>
-			<RightSideBar />
-		</div>
+*/
+
+const SpacePage = () => {
+	const [username, setUsername] = useState("");
+	const [color, setColor] = useState("");
+
+	const handleClick = () => {
+		console.log("clicked")
+	};
+
+	return (
+		<div className="flex h-screen overflow-hidden">
+      <main className="flex-grow flex flex-col overflow-hidden relative">
+        <div className="relative flex-grow">
+          {username ? (
+            <>
+              <CursorContainer username={username} color={color} />
+              <div className="absolute inset-0 z-30 pointer-events-none">
+                <button
+                  type="button"
+                  className="bg-black text-white ml-20 pointer-events-auto"
+                  onClick={handleClick}
+                >
+                  Click
+                </button>
+              </div>
+            </>
+          ) : (
+            <SetNickname onSubmit={setUsername} setColorProp={setColor} />
+          )}
+        </div>
+        <BottomBar />
+      </main>
+      <RightSideBar />
+    </div>
 	);
 };
 
--- components/space/CursorContainer.tsx
+++ components/space/CursorContainer.tsx
@@ -5,121 +5,120 @@ import { useEffect, useRef, useState } from "react";
 import useWebSocket from "react-use-websocket";
 import throttle from "lodash.throttle";
 import { Cursor } from "./Cursor";
+
 interface UserState {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 }
+
 interface User {
-	username: string;
-	state: UserState;
+  username: string;
+  state: UserState;
 }
+
 interface Users {
-	[uuid: string]: User;
+  [uuid: string]: User;
 }
+
 interface HomeProps {
-	username: string;
-	color: string;
+  username: string;
+  color: string;
 }
 
 const renderCursors = (users: Users, color: string) => {
-	return Object.keys(users).map((uuid) => {
-		const user = users[uuid];
-		return (
-			<Cursor key={uuid} point={[user.state.x, user.state.y]} color={color} />
-		);
-	});
+  return Object.keys(users).map((uuid) => {
+    const user = users[uuid];
+    return (
+      <Cursor key={uuid} point={[user.state.x, user.state.y]} color={color} />
+    );
+  });
 };
 
 const renderUsersList = (users: Users) => {
-	return (
-		<ul>
-			{Object.keys(users).map((uuid) => {
-				return <li key={uuid}>{users[uuid].username}</li>;
-			})}
-		</ul>
-	);
+  return (
+    <ul>
+      {Object.keys(users).map((uuid) => {
+        return <li key={uuid}>{users[uuid].username}</li>;
+      })}
+    </ul>
+  );
 };
 
 const CursorContainer: React.FC<HomeProps> = ({ username, color }) => {
-	const [otherUsers, setOtherUsers] = useState<Users>({});
+  const [otherUsers, setOtherUsers] = useState<Users>({});
+  const WS_URL = "ws://localhost:8000";
 
-	const WS_URL = "ws://localhost:8000";
+  const containerRef = useRef<HTMLDivElement>(null);
 
-	const containerRef = useRef<HTMLDivElement>(null);
+  const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
+    queryParams: { username },
+  });
 
-	const { sendJsonMessage, lastJsonMessage } = useWebSocket(WS_URL, {
-		queryParams: { username },
-	});
+  const THROTTLE = 30;
+  const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
 
-	const THROTTLE = 10;
+  useEffect(() => {
+    sendJsonMessage({
+      x: 0,
+      y: 0,
+    });
 
-	const sendJsonMessageThrottled = useRef(throttle(sendJsonMessage, THROTTLE));
+    const onmousemove = (e: MouseEvent) => {
+      if (containerRef.current) {
+        const rect = containerRef.current.getBoundingClientRect();
+        sendJsonMessageThrottled.current({
+          x: e.clientX - rect.left,
+          y: e.clientY - rect.top,
+        });
+      }
+    };
 
-	useEffect(() => {
-		sendJsonMessage({
-			x: 0,
-			y: 0,
-		});
+    const container = containerRef.current;
 
-		const handleMouseMove = (e: MouseEvent) => {
-			if (containerRef.current) {
-				const rect = containerRef.current.getBoundingClientRect();
-				sendJsonMessageThrottled.current({
-					x: e.clientX - rect.left,
-					y: e.clientY - rect.top,
-				});
-			}
-		};
+    if (container) {
+      container.addEventListener("mousemove", onmousemove);
+    }
 
-		const container = containerRef.current;
+    return () => {
+      if (container) {
+        container.removeEventListener("mousemove", onmousemove);
+      }
+    };
+  }, [sendJsonMessage]);
 
-		if (container) {
-			container.addEventListener("mousemove", handleMouseMove);
-		}
+  useEffect(() => {
+    if (lastJsonMessage) {
+      const users = lastJsonMessage as Users;
+      const filteredUsers = Object.keys(users).reduce<Users>((acc, uuid) => {
+        if (users[uuid].username !== username) {
+          acc[uuid] = users[uuid];
+        }
+        return acc;
+      }, {});
+      setOtherUsers(filteredUsers);
+    }
+  }, [lastJsonMessage, username]);
 
-		return () => {
-			if (container) {
-				container.removeEventListener("mousemove", handleMouseMove);
-			}
-		};
-	}, [sendJsonMessage]);
-
-	useEffect(() => {
-		if (lastJsonMessage) {
-			const users = lastJsonMessage as Users;
-			const filteredUsers = Object.keys(users).reduce<Users>((acc, uuid) => {
-				if (users[uuid].username !== username) {
-					acc[uuid] = users[uuid];
-				}
-				return acc;
-			}, {});
-			setOtherUsers(filteredUsers);
-		}
-	}, [lastJsonMessage, username]);
-
-	return (
-		<div
-			ref={containerRef}
-			className="
-        absolute 
-        inset-0
-        bg-transparent
-        z-20   
-        text-white
-      "
-		>
-			<h1>Hello, {username}</h1>
-			<p>Current users:</p>
-			{Object.keys(otherUsers).length === 0 ? (
-				<p>No other users online.</p>
-			) : (
-				<>
-					{renderUsersList(otherUsers)}
-					{renderCursors(otherUsers, color)}
-				</>
-			)}
-		</div>
-	);
+  return (
+    <div className="absolute inset-0 bg-transparent z-20 pointer-events-none">
+      <div
+        ref={containerRef}
+        className="absolute inset-0 z-20"
+        style={{ pointerEvents: "none" }}
+      >
+        <h1>Hello, {username}</h1>
+        <p>Current users:</p>
+        {Object.keys(otherUsers).length === 0 ? (
+          <p>No other users online.</p>
+        ) : (
+          <>
+            {renderUsersList(otherUsers)}
+            {renderCursors(otherUsers, color)}
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CursorContainer;
