007cc3c3310b118f68c04f2aefee7506536a1c4c
Sunil Paul <sunilpaul16@gmail.com>
2024-08-13 15:41:30 +0100

    feat: Add AddMembers component for creating new spaces and adding members

---
+++ components/dashboard/popupcreatspace/CreateNewSpace.tsx
@@ -0,0 +1,12 @@
+import AddMembers from "./AddMembers";
+
+export const CreateNewSpace = () => {
+	return (
+		<div>
+			<div>
+				<h1 className="flex justify-center text-5xl mb-5 ">Create New Space</h1>
+			</div>
+			<AddMembers />
+		</div>
+	);
+};
