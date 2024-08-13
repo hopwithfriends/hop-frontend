9c050d8e2e55dfd76ba7e2f5926e0c0d271d9889
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-13 16:09:17 +0100

    feat(auth): implement custom sign-up/sign-in pages w/ stack components

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { SignIn } from "@stackframe/stack";
+
+export default function SignInPage() {
+	return (
+		<div className="flex justify-center items-center h-screen">
+			<SignIn />
+		</div>
+	);
+}
