46d323b836644cc44fdd9c54c84972c94ed4fd82
Isabella <idavila2021@gmail.com>
2024-08-13 23:27:45 +0100

    style(dash): setUser form style

--- components/space/SetNickname.tsx
+++ components/space/SetNickname.tsx
@@ -4,6 +4,7 @@
 import React, { useState, useEffect } from "react";
 import randomColor from "randomcolor";
 import useWebSocket from "react-use-websocket";
+import Image from "next/image";
 interface NicknameProps {
 	onSubmit: (username: string) => void;
 	setColorProp: (color: string) => void;
@@ -53,9 +54,16 @@ const SetNickname: React.FC<NicknameProps> = ({
 	};
 
 	return (
-		<>
-			<div className="bg-gray-600 text-white rounded-xl shadow-lg p-6 w-96 mx-auto mt-[25%] ml-[41%] absolute z-50 ">
-				<p className="text-lg font-semibold mb-4">Set your nickname</p>
+		<div className="relative text-white rounded-xl shadow-lg p-6 w-96 mx-auto mt-[25%] z-50 flex flex-col items-center justify-center">
+			<Image
+				src="/images/Logo.webp"
+				alt="Logo"
+				width={700}
+				height={700}
+				priority
+				className="absolute z-0"
+			/>
+			<div className="absolute z-10 rounded-xl p-6 shadow-lg w-full mt-[67%]">
 				<form onSubmit={handleSubmit} className="flex flex-col space-y-4">
 					<input
 						type="text"
@@ -63,17 +71,17 @@ const SetNickname: React.FC<NicknameProps> = ({
 						placeholder="Enter your nickname..."
 						onChange={(e) => setUsername(e.target.value)}
 						maxLength={6}
-						className="border border-gray-600 rounded-md p-2 bg-gray-900 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500"
+						className="border border-gray-600 rounded-md p-2 bg-gray-900 text-white placeholder-white focus:outline-none focus:ring-2 focus:ring-pink-500"
 					/>
 					<button
 						type="submit"
-						className="bg-gray-900 hover:bg-gray-950 text-white rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-gray-500"
+						className="bg-pink-400 hover:bg-pink-500 text-white rounded-md py-2 px-4 focus:outline-none focus:ring-2 focus:ring-pink-600"
 					>
-						Submit
+						Hop In
 					</button>
 				</form>
 			</div>
-		</>
+		</div>
 	);
 };
 
