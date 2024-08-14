f864436c2ea180f27cf94d674e84bf5858cd29d5
Isabella <idavila2021@gmail.com>
2024-08-14 01:49:46 +0100

    refactor(leftSideBar): map through online users array mocked

--- components/space/SelectCursor.tsx
+++ components/space/SelectCursor.tsx
@@ -1,26 +1,24 @@
 import type React from "react";
 import Image from "next/image";
 import { useState } from "react";
-
 interface SelectProps {
 	setSelectedCursor: (cursor: string) => void;
 }
 
 const SelectCursor: React.FC<SelectProps> = ({ setSelectedCursor }) => {
-  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+	const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-  
 	const handleClick = (src: string) => {
 		setSelectedCursor(src);
 
-    setSelectedImage(src);
+		setSelectedImage(src);
 	};
 
 	return (
 		<div className="absolute bg-slate-500 flex flex-row pt-2 pb-2 pr-3">
 			<Image
 				onClick={() => handleClick("/norm.png")}
-				className={`ml-3 w-[25%] h-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/norm.png" ? 'border-2 border-green-500' : ''}`}
+				className={`ml-3 w-[25%] h-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/norm.png" ? "border-3 border-green-500" : ""}`}
 				src={"/norm.png"}
 				alt={"cursor"}
 				width={50}
@@ -28,7 +26,7 @@ const SelectCursor: React.FC<SelectProps> = ({ setSelectedCursor }) => {
 			/>
 			<Image
 				onClick={() => handleClick("/monkey.jpg")}
-				className={`ml-3 w-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/monkey.jpg" ? 'border-2 border-green-500' : ''}`}
+				className={`ml-3 w-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/monkey.jpg" ? "border-spacing-3 border-green-500" : ""}`}
 				src={"/monkey.jpg"}
 				alt={"cursor"}
 				width={50}
@@ -36,7 +34,7 @@ const SelectCursor: React.FC<SelectProps> = ({ setSelectedCursor }) => {
 			/>
 			<Image
 				onClick={() => handleClick("/ani.jpg")}
-				className={`ml-3 w-[25%] h-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/ani.jpg" ? 'border-2 border-green-500' : ''}`}
+				className={`ml-3 w-[25%] h-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/ani.jpg" ? "border-3 border-green-500" : ""}`}
 				src={"/ani.jpg"}
 				alt={"cursor"}
 				width={50}
@@ -44,7 +42,7 @@ const SelectCursor: React.FC<SelectProps> = ({ setSelectedCursor }) => {
 			/>
 			<Image
 				onClick={() => handleClick("/emoji.jpg")}
-				className= {`ml-3 w-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/emoji.jpg" ? 'border-2 border-green-500' : ''}`}
+				className={`ml-3 w-[100%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/emoji.jpg" ? "border-3 border-green-500" : ""}`}
 				src={"/emoji.jpg"}
 				alt={"cursor"}
 				width={50}
@@ -52,7 +50,7 @@ const SelectCursor: React.FC<SelectProps> = ({ setSelectedCursor }) => {
 			/>
 			<Image
 				onClick={() => handleClick("/partick.jpg")}
-				className={`ml-3 w-[25%] h-[80%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/partick.jpg" ? 'border-2 border-green-500' : ''}`}
+				className={`ml-3 w-[25%] h-[80%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/partick.jpg" ? "border-3 border-green-500" : ""}`}
 				src={"/patrick.jpg"}
 				alt={"cursor"}
 				width={50}
@@ -60,7 +58,7 @@ const SelectCursor: React.FC<SelectProps> = ({ setSelectedCursor }) => {
 			/>
 			<Image
 				onClick={() => handleClick("/skate.jpg")}
-				className= {`ml-3 w-[25%] h-[80%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/skate.jpg" ? 'border-2 border-green-500' : ''}`}
+				className={`ml-3 w-[25%] h-[80%] transition-transform transform hover:scale-105 border-2 border-slate-500${selectedImage === "/skate.jpg" ? "border-3 border-green-500" : ""}`}
 				src={"/skate.jpg"}
 				alt={"cursor"}
 				width={50}
