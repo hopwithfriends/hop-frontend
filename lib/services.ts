b8631443edd447b3e83c2128196b1a16f1b8ea3e
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-14 13:01:23 +0100

    refactor: improve error handling in api service class

--- lib/services.ts
+++ lib/services.ts
@@ -1,12 +1,14 @@
 export class ApiService {
-	private BASE_URL = "localhost:8080/api";
+	private BASE_URL = "http://localhost:8080/api";
 	private accessToken = "";
 	private refreshToken = "";
 
-	constructor(accessToken: string, refreshToken: string) {
+	constructor(accessToken: string | null, refreshToken: string | null) {
 		if (!accessToken || !refreshToken) {
 			throw new Error("Access/refresh token are required for the ApiSerice");
 		}
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
 	}
 
 	private getHeaders() {
@@ -20,31 +22,43 @@ export class ApiService {
 		const res = await fetch(`${this.BASE_URL}${endpoint}`, {
 			headers: this.getHeaders(),
 		});
-		return await res.json();
+		if (res.ok) {
+			return await res.json();
+		}
+		throw new Error(`HTTP Error: ${res.status}`);
 	}
 
-	async post(endpoint: string, data) {
+	async post(endpoint: string, data: unknown) {
 		const res = await fetch(`${this.BASE_URL}${endpoint}`, {
 			method: "POST",
 			headers: this.getHeaders(),
 			body: JSON.stringify(data),
 		});
-		return await res.json();
+		if (res.ok) {
+			return await res.json();
+		}
+		throw new Error(`HTTP Error: ${res.status}`);
 	}
 
-	async put(endpoint: string, data) {
+	async put(endpoint: string, data: unknown) {
 		const res = await fetch(`${this.BASE_URL}${endpoint}`, {
 			method: "PUT",
 			headers: this.getHeaders(),
 			body: JSON.stringify(data),
 		});
-		return await res.json();
+		if (res.ok) {
+			return await res.json();
+		}
+		throw new Error(`HTTP Error: ${res.status}`);
 	}
-	async delete(endpoint: string, data) {
+	async delete(endpoint: string, data: unknown) {
 		const res = await fetch(`${this.BASE_URL}${endpoint}`, {
 			method: "DELETE",
 			headers: this.getHeaders(),
 		});
-		return await res.json();
+		if (res.ok) {
+			return await res.json();
+		}
+		throw new Error(`HTTP Error: ${res.status}`);
 	}
 }
