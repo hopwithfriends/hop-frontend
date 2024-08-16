f191d7df5c99e640dceb535555d961e381b2e530
Isabella <idavila2021@gmail.com>
2024-08-16 23:49:51 +0100

    fix(nextconfig/avitar): coudinary and remove class function

--- next.config.mjs
+++ next.config.mjs
@@ -4,29 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "tse2.mm.bing.net",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
-      },
-      {
-        protocol: "https",
-        hostname: "tse3.explicit.bing.net",
-      },{
-        protocol: "https",
-        hostname: "i.imgur.com",
-      },{
-        protocol: "https",
-        hostname: "imgur.com",
-      },
-      {protocol: "https",
-        hostname: "res.cloudinary.com"
-      }
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "res.cloudinary.com" }],
   },
 };
 
