e3917b85d1208c1c39ee009ce1800c4b5258378f
Haroon Bakhsh <h@hbak.co>
2024-08-18 18:13:32 +0100

    feat: implement real-time status (#38)

--- components/layout/OnlineFriendsContainer.tsx
+++ components/layout/OnlineFriendsContainer.tsx
@@ -1,34 +1,16 @@
 "use client";
 
+import type { FriendsType } from "@app/context/SocketProvider";
 import OnlineFriend from "@components/friends/OnlineFriend";
-import { useState } from "react";
-
-interface Friend {
-	id: string;
-	nickname: string;
-	username: string;
-	profilePicture?: string;
-	isOnline?: boolean;
-	currentRoom?: string;
-}
 
 interface OnlineFriendsContainerProps {
-	friends: Friend[];
+	friends: FriendsType[];
 }
 
 const OnlineFriendsContainer: React.FC<OnlineFriendsContainerProps> = ({
 	friends,
 }) => {
-	const onlineFriends = friends.filter((friend) => friend.isOnline);
-
-	const [onlineUser, setOnlineUser] = useState([
-		{ id: "1", spaceName: "Space 1", username: "User 1", pfp: "/ani.jpg" },
-		{ id: "2", spaceName: "Space 2", username: "User 2", pfp: "/emoji.jpg" },
-		{ id: "3", spaceName: "Space 3", username: "User 3", pfp: "/patrick.jpg" },
-		{ id: "4", spaceName: "Space 4", username: "User 4", pfp: "/monkey.jpg" },
-		{ id: "5", spaceName: "Space 5", username: "User 5", pfp: "/skate.jpg" },
-		{ id: "6", spaceName: "Space 6", username: "User 6", pfp: "/PFP.jpg" },
-	]); // mock, fetch from backend later
+	const onlineFriends = friends.filter((friend) => friend.status);
 
 	return (
 		<div className="bg-gray-800 w-full h-full p-4 rounded-xl shadow-lg border border-gray-700 overflow-auto">
@@ -36,11 +18,11 @@ const OnlineFriendsContainer: React.FC<OnlineFriendsContainerProps> = ({
 				{onlineFriends.map((friend) => (
 					<li key={friend.id}>
 						<OnlineFriend
+							id={friend.id}
 							nickname={friend.nickname}
 							username={friend.username}
 							profilePicture={friend.profilePicture}
-							currentRoom={friend.currentRoom}
-							id={""}
+							currentRoom={friend.status.spaceId}
 						/>
 					</li>
 				))}
