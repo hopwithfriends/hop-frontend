b561c10d3bb891b298ab5bb0d9b61d3674ad601b
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-20 14:06:04 +0100

    feat: implement user type for service return

--- lib/servicesMethods.ts
+++ lib/servicesMethods.ts
@@ -1,71 +1,79 @@
-import { ApiService } from './services';
+import { ApiService } from "./services";
+
+export type UserType = {
+	id: string;
+	username: string;
+	nickname: string;
+	profilePicture: string;
+	createdAt: string;
+};
 
 export class ServiceMethods {
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  [x: string]: any;
-  fetchFriends() {
-    throw new Error('Method not implemented.');
-  }
-  private accessToken = '';
-  private refreshToken = '';
-  constructor(accessToken: string, refreshToken: string) {
-    this.accessToken = accessToken;
-    this.refreshToken = refreshToken;
-  }
-  apiService() {
-    return new ApiService(this.accessToken, this.refreshToken);
-  }
-  async fetchUser() {
-    return this.apiService().get('/user');
-  }
-  async fetchUpdateUser(
-    username: string,
-    nickname: string,
-    profilePicture: string
-  ) {
-    return this.apiService().put('/user', {
-      username,
-      nickname,
-      profilePicture
-    });
-  }
+	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	[x: string]: any;
+	fetchFriends() {
+		throw new Error("Method not implemented.");
+	}
+	private accessToken = "";
+	private refreshToken = "";
+	constructor(accessToken: string, refreshToken: string) {
+		this.accessToken = accessToken;
+		this.refreshToken = refreshToken;
+	}
+	apiService() {
+		return new ApiService(this.accessToken, this.refreshToken);
+	}
+	async fetchUser(): Promise<UserType> {
+		return this.apiService().get("/user");
+	}
+	async fetchUpdateUser(
+		username: string,
+		nickname: string,
+		profilePicture: string,
+	) {
+		return this.apiService().put("/user", {
+			username,
+			nickname,
+			profilePicture,
+		});
+	}
 
-  async fetchAllFriends() {
-    return this.apiService().get('/user/friend');
-  }
-  async fetchAddFriend(friendId: string) {
-    return this.apiService().post(`/user/friend/${friendId}`, {});
-  }
-  async fetchRemoveFriend(friendId: string) {
-    return this.apiService().delete(`/user/friend/${friendId}`, {});
-  }
+	async fetchAllFriends() {
+		return this.apiService().get("/user/friend");
+	}
+	async fetchAddFriend(friendId: string) {
+		return this.apiService().post(`/user/friend/${friendId}`, {});
+	}
+	async fetchRemoveFriend(friendId: string) {
+		return this.apiService().delete(`/user/friend/${friendId}`, {});
+	}
 
-  async fetchCreateSpace(
-    name: string,
-    theme: string,
-    id: string,
-    password: string
-  ) {
-    return this.apiService().post('/space', { name, theme, id, password });
-  }
-  async fetchRemoveSpace(spaceId: string) {
-    return this.apiService().delete(`/space/${spaceId}`, {});
-  }
-  async fetchAddFriendToSpace(spaceId: string, friendId: string, role: string) {
-    return this.apiService().post('/space/addUser', {
-      spaceId,
-      role,
-      friendId
-    });
-  }
+	async fetchCreateSpace(
+		name: string,
+		theme: string,
+		id: string,
+		password: string,
+	) {
+		return this.apiService().post("/space", { name, theme, id, password });
+	}
+	async fetchRemoveSpace(spaceId: string) {
+		return this.apiService().delete(`/space/${spaceId}`, {});
+	}
+	async fetchAddFriendToSpace(spaceId: string, friendId: string, role: string) {
+		return this.apiService().post("/space/addUser", {
+			spaceId,
+			role,
+			friendId,
+		});
+	}
 
-  async fetchUserSpaces() {
-    return this.apiService().get('/space/myspaces');
-  }
-  async fetchInvitedSpace() {
-    return this.apiService().get('/space/invitedspaces');
-  }
-  async fetchSpaceById(spaceId: string) {
-    return this.apiService().get(`/spaceId/${spaceId}`);
-  }
+	async fetchUserSpaces() {
+		return this.apiService().get("/space/myspaces");
+	}
+	async fetchInvitedSpace() {
+		return this.apiService().get("/space/invitedspaces");
+	}
+	async fetchSpaceById(spaceId: string) {
+		return this.apiService().get(`/spaceId/${spaceId}`);
+	}
 }
