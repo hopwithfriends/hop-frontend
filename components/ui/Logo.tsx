bd2463338a32220d5ec397d0b814fe0e0864c94b
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-20 14:02:03 +0100

    feat: implement new hop logo in sidebar

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -6,12 +6,12 @@ interface LogoProps {
 export const Logo: React.FC<LogoProps> = () => {
 	return (
 		<Image
-			src="/images/Logo.webp"
+			src="/images/hop-box-wordmark.png"
 			alt="Logo"
-			width={100}
+			width={150}
 			height={100}
 			priority
-			className="mx-auto rounded-full mb-5 object-contain"
+			// className="mx-auto rounded-full mb-5 object-contain"
 		/>
 	);
 };
