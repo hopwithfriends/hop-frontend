ca6870528f3cf47a5b57d34f2ab316b88c234892
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-20 13:51:48 +0100

    refactor: change dashboard background colour

--- app/(hop)/dashboard/page.tsx
+++ app/(hop)/dashboard/page.tsx
@@ -4,18 +4,11 @@ import type React from "react";
 import { useUser } from "@stackframe/stack";
 import SpaceContainer from "@components/dashboard/SpaceContainer";
 import { useEffect, useState } from "react";
-import { ServiceMethods } from "@lib/servicesMethods";
-
-interface UserDataType {
-	userId: string;
-	username: string;
-	nickname: string;
-	profilePicture: string;
-}
+import { ServiceMethods, type UserType } from "@lib/servicesMethods";
 
 const Dashboard: React.FC = () => {
 	const user = useUser({ or: "redirect" });
-	const [userData, setUserData] = useState<UserDataType>();
+	const [userData, setUserData] = useState<UserType>();
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
@@ -39,11 +32,13 @@ const Dashboard: React.FC = () => {
 	const screen: string = "/placeholder.jpg"; // ??
 
 	return (
-		<div className="flex bg-gray-700 text-white h-screen">
+		<div className="flex bg-hop-secondary-bg text-white h-screen">
 			<SpaceContainer
 				screen={screen}
-
-				pfp={userData?.profilePicture || "https://res.cloudinary.com/dksp40fgp/image/upload/v1723767101/oklypy7hrfwisbjnxwaw.png"}
+				pfp={
+					userData?.profilePicture ||
+					"https://res.cloudinary.com/dksp40fgp/image/upload/v1723767101/oklypy7hrfwisbjnxwaw.png"
+				}
 				link={link}
 				members={members}
 				realUsername={userData?.username || ""}
