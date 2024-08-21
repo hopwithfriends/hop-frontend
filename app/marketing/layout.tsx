ebd27a2524c48c5579f5f92c3254d8679ac54ac6
Manuel Rosa <96manuelr@gmail.com>
2024-08-21 11:39:03 +0100

    fix(market): removed market (#72)

--- app/marketing/layout.tsx
+++
@@ -1,9 +0,0 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import React from 'react';
-import "@styles/globals.css";
-
-const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div> {children} </div>
-};
-
-export default MarketingLayout;
